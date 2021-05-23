ecf5aa5e401474a4cc0facd4a5f4fda6d1dceb49
Lao_Dong <dongyanbogame@hotmail.com>
2021-05-23 17:10:57 +0800

    建立图形界面

--- Project1/dorand.cpp
+++ Project1/dorand.cpp
@@ -1,13 +1,13 @@
 #include "cs.h"
 #include"func.h"
 void dorand(int* map) {
-	srand((unsigned int)time(0));
 	if (docheck(map) == 0) {
 		while (true)
 		{
 			int a = rand() % M;
 			if (map[a] == 0) {
 				map[a] = 2;
+				break;
 			}
 		}
 	}
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,16 +1,40 @@
 #include"cs.h"
 #include"func.h"
+void create_map(void);
+int map[4][4] = { 0 };
 int main(void)
 {
-	int map[4][4] = { 0 };
+	srand((unsigned int)time(0));
 	int* pmap = &map[0][0];
 	int e = docheck(pmap);
 	dorand(pmap);
+	create_map();
+	for (int j = 0; j < 4; j++)
+	{
+		for (int i = 0; i < 4; i++) {
+			printf("%d ", map[j][i]);
+		}
+		printf("\n");
+	}
 	if (e != 0) {
 		printf("you lose the game!");
-		show_map(pmap);
 		system("pause");
 		return 0;
 	}
+	getch();
+	return 0;
 
+}
+void create_map(void) {
+	initgraph(1120, 630);
+	PIMAGE bak = newimage();
+	getimage(bak, ".\\img\\backblock.png");
+	putimage(0, 0, bak);
+	delimage(bak);
+	PIMAGE n0 = newimage();
+	getimage(n0, A_);
+	PIMAGE n1 = newimage(), n2 = newimage(), n3 = newimage(), n4 = newimage(), n5 = newimage(), n6 = newimage(), n7 = newimage(), n8 = newimage(), n9 = newimage();
+	PIMAGE n10 = newimage(), n11 = newimage(), n12 = newimage(), n13 = newimage(), n14 = newimage(), n15 = newimage();
+	putimage(36, 164, n0);
+	putimage(206, 164, n1);
 }
