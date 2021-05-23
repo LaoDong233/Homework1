373ce9e40bf7de8f2032c9acfcfd2b7ae1358e20
Lao_Dong <dongyanbogame@hotmail.com>
2021-05-23 17:10:39 +0800

    定义2048常量

--- Project1/cs.h
+++ Project1/cs.h
@@ -3,4 +3,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#include<graphics.h>
+#include <graphics.h>
+#define BLOCK ".\\img\\block.png"
+#define A_ ".\\img\\2.png"
+#define B_ ".\\img\\4.png"
+#define C_ ".\\img\\8.png"
+#define D_ ".\\img\\16.png"
+#define E_ ".\\img\\32.png"
+#define F_ ".\\img\\64.png"
+#define G_ ".\\img\\128.png"
+#define H_ ".\\img\\256.png"
+#define I_ ".\\img\\512.png"
+#define J_ ".\\img\\1024.png"
+#define K_ ".\\img\\2048.png"
